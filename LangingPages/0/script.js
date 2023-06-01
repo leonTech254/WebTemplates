e77edd9be821771699fee854252c2ed97a5797c6
leonTech254 <martinleontech23@gmail.com>
2023-06-01 17:46:10 +0300

    --landing pages

---
+++ LangingPages/0/script.js
@@ -0,0 +1,17 @@
+function animateHeight() {
+var element = document.querySelector('.navItems');
+         
+        
+    
+ 
+     
+
+    element.classList.toggle("activeMenu");
+      
+
+        
+    
+
+
+ 
+}
